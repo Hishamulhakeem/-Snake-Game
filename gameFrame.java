7ac40abcb2185e13a87205f52910f0a5f8348993
Hisham ul hakeem A <155228704+Hishamulhakeem@users.noreply.github.com>
2025-07-10 19:03:10 +0530

    Add files via upload

---
+++ gameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class gameFrame extends JFrame{
+
+	gameFrame(){
+		
+		this.add(new gamePannel());
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+	
+}
